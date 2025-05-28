1ff2db44b2b04a49b2ae45f5a9672b8b6c1d0f3e
Seojun <auddml321@hanyang.ac.kr>
2025-05-28 16:51:33 +0900

    실습 2-18: TestController 작성

--- src/main/java/com/example/TodoApp/controller/TestController.java
+++ src/main/java/com/example/TodoApp/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.example.TodoApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.TodoApp.dto.ResponseDTO;
+import com.example.TodoApp.dto.TestReqeustBodyDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
-    @GetMapping
-    public String testController() {
-        return "Hello World";
+    @GetMapping("/responsebody")
+    public ResponseDTO<String> testControllerRequestParam() {
+        List<String> list = new ArrayList<>();
+        list.add("Hi, I'm responseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
     }
 }
