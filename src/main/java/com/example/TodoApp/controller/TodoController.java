d3410f84b42cc982dcad1c40f88f63a14a56a3d0
Seojun <auddml321@hanyang.ac.kr>
2025-05-28 17:47:26 +0900

    실습 2-20: TodoController 작성

--- src/main/java/com/example/TodoApp/controller/TodoController.java
+++ src/main/java/com/example/TodoApp/controller/TodoController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
     @GetMapping("/list")
     public ResponseEntity<?> testTodo (){
-        ResponseDTO responseDTO = new ResponseDTO();
-        return ResponseEntity.ok().build();
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(responseDTO);
     }
 }
