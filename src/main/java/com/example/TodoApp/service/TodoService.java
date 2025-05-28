fd7670edd8b1c3881ef0507e55b53841ecc06463
Seojun <auddml321@hanyang.ac.kr>
2025-05-28 17:47:32 +0900

    실습 2-21: TodoController 작성

---
+++ src/main/java/com/example/TodoApp/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.example.TodoApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    public String testService() {
+        return "Hello World";
+    }
+}
