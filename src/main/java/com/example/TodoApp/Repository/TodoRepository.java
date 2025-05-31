f914dc7862d7b3022af79f1533886d7376870775
Seojun <auddml321@hanyang.ac.kr>
2025-05-31 15:14:35 +0900

    실습 2-27: TodoRepository 완성

---
+++ src/main/java/com/example/TodoApp/Repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.example.TodoApp.Repository;
+
+import com.example.TodoApp.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+}
