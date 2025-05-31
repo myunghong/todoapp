c4c7648ad3c209fa30e1d48b4c4fa6dac8ecd766
Seojun <auddml321@hanyang.ac.kr>
2025-05-31 15:25:23 +0900

    실습 2-28: Todoservice와 TodoRepository 완성

--- src/main/java/com/example/TodoApp/service/TodoService.java
+++ src/main/java/com/example/TodoApp/service/TodoService.java
@@ -1,11 +1,18 @@
 package com.example.TodoApp.service;
 
+import com.example.TodoApp.Repository.TodoRepository;
+import com.example.TodoApp.model.TodoEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
+    private TodoRepository todoRepository;
+
     public String testService() {
-        return "Hello World";
+        TodoEntity entity = TodoEntity.builder().title("스프링 공부하기").build();
+        todoRepository.save(entity);
+        TodoEntity savedEntity = todoRepository.findById(entity.getId()).orElse(null);
+        return savedEntity.getTitle();
     }
 }
