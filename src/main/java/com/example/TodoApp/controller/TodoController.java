75beaaa745960e7dfe8a9e70ce104381d7ecddd8
Seojun <auddml321@hanyang.ac.kr>
2025-06-03 20:54:19 +0900

    실습 2-34: createtodo 완성

--- src/main/java/com/example/TodoApp/controller/TodoController.java
+++ src/main/java/com/example/TodoApp/controller/TodoController.java
@@ -1,15 +1,20 @@
 package com.example.TodoApp.controller;
 
 import com.example.TodoApp.dto.ResponseDTO;
+import com.example.TodoApp.dto.TodoDTO;
+import com.example.TodoApp.entity.TodoEntity;
 import com.example.TodoApp.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.codec.ServerSentEvent.builder;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +22,7 @@ import java.util.List;
 public class TodoController {
 
     private final TodoService todoService;
+    private final RestClient.Builder builder;
 
     @GetMapping("/test")
     public ResponseEntity<?> testTodo (){
@@ -25,5 +31,23 @@ public class TodoController {
         list.add(str);
         ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(responseDTO);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+        try{
+            String temporaryUserUId = "temporaryUserUId";
+            TodoEntity entity = TodoDTO.toEntity(dto);
+            entity.setId(null);
+            entity.setUserId(temporaryUserUId);
+            List<TodoEntity> entities = todoService.create(entity);
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.ok().body(response);
+        }
     }
 }
